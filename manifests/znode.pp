c741bd61924dc9b708ddeb2e8e48dd46f8884b9f
liamjbennett <lbennett@opentable.com>
2014-08-05 13:49:45 +0100

    fixing znode

--- manifests/znode.pp
+++ manifests/znode.pp
@@ -5,13 +5,13 @@ define zookeeper::znode(
   
   if $ensure == 'present' {
     exec { "create znode ${name}":
-      command => "echo \"create /${name} '${assosiation}'\" | ${zookeeper::install_dir}/bin/zkCli.sh",
-      unless  => "if [[ `echo 'ls /' | /opt/zookeeper/bin/zkCli.sh | tail -n2 | head -n1` == *${name} ]]; then exit 0; else exit 1"
+      command => "/bin/echo \"create /${name} '${assosiation}'\" | ${zookeeper::install_dir}/bin/zkCli.sh",
+      unless  => "/bin/bash -c \"if [[ `echo 'ls /' | /opt/zookeeper/bin/zkCli.sh | tail -n2 | head -n1` == *${name} ]]; then exit 0; else exit 1\""
     }
   } else {
     exec { "delete znode ${name}":
-      command => "echo \"rmr /${name}\" | ${zookeeper::install_dir}/bin/zkCli.sh",
-      unless  => "if [[ `echo 'ls /' | /opt/zookeeper/bin/zkCli.sh | tail -n2 | head -n1` == *${name} ]]; then exit 1; else exit 0"
+      command => "/bin/echo \"rmr /${name}\" | ${zookeeper::install_dir}/bin/zkCli.sh",
+      unless  => "/bin/bash -c \"if [[ `echo 'ls /' | /opt/zookeeper/bin/zkCli.sh | tail -n2 | head -n1` == *${name} ]]; then exit 1; else exit 0\""
     }
   }
 }
